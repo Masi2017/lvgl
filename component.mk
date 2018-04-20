9d23002b575eba12fe058fa4175bd9bd45f833a3
masi <masy.m1980@gmail.com>
2018-04-20 16:40:23 -0400

    test for pull request

---
+++ component.mk
@@ -0,0 +1,6 @@
+#
+# Main Makefile. This is basically the same as a component makefile.
+#
+# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+COMPONENT_ADD_INCLUDEDIRS := . lv_core lv_draw lv_hal lv_misc lv_objx lv_themes lv_misc/lv_fonts
+COMPONENT_SRCDIRS := . lv_core lv_draw lv_hal lv_misc lv_objx lv_themes lv_misc/lv_fonts
